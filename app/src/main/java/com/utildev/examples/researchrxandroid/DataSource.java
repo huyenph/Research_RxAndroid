cdcb28719bd35a52ef97686b1092bb43073a7bdc
Huyen Pham <huyen.phamhoang96@gmail.com>
2019-07-08 14:55:27 +0700

    creating observable object

---
+++ app/src/main/java/com/utildev/examples/researchrxandroid/DataSource.java
@@ -0,0 +1,21 @@
+package com.utildev.examples.researchrxandroid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class DataSource {
+    static List<Task> createTaskList() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Task 1", true, 0));
+        tasks.add(new Task("Task 2", false, 1));
+        tasks.add(new Task("Task 3", true, 2));
+        tasks.add(new Task("Task 4", false, 3));
+        tasks.add(new Task("Task 5", true, 4));
+        tasks.add(new Task("Task 6", false, 5));
+        tasks.add(new Task("Task 7", true, 6));
+        tasks.add(new Task("Task 8", false, 7));
+        tasks.add(new Task("Task 9", true, 8));
+        tasks.add(new Task("Task 10", false, 9));
+        return tasks;
+    }
+}
--- app/src/main/java/com/utildev/examples/researchrxandroid/MainActivity.java
+++ app/src/main/java/com/utildev/examples/researchrxandroid/MainActivity.java
@@ -3,6 +3,14 @@ package com.utildev.examples.researchrxandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTaskList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) {
+                        Log.d("aaa", "test: " + Thread.currentThread().getName());
+                        Log.d("aaa", "test: " + task.getDescription());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("aaa", "onSubscribe: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d("aaa", "onNext: " + Thread.currentThread().getName());
+                Log.d("aaa", "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("aaa", "onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("aaa", "onComplete: ");
+            }
+        });
     }
 }
