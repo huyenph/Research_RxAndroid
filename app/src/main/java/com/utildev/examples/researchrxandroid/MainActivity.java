fec0b054107b77b8849b37e1e14e628db59420ce
Huyen Pham <huyen.phamhoang96@gmail.com>
2019-07-08 17:17:58 +0700

    flowable

--- app/src/main/java/com/utildev/examples/researchrxandroid/MainActivity.java
+++ app/src/main/java/com/utildev/examples/researchrxandroid/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableSubscriber;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -19,45 +23,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<Task> taskObservable = Observable
-                .fromIterable(DataSource.createTaskList())
-                .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() {
+        // Creating Observable
+//        Observable<Task> taskObservable = Observable
+//                .fromIterable(DataSource.createTaskList())
+//                .subscribeOn(Schedulers.io())
+//                .filter(new Predicate<Task>() {
+//                    @Override
+//                    public boolean test(Task task) {
+//                        Log.d("aaa", "test: " + Thread.currentThread().getName());
+//                        Log.d("aaa", "test: " + task.getDescription());
+//                        try {
+//                            Thread.sleep(1000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
+//                        return task.isComplete();
+//                    }
+//                })
+//                .observeOn(AndroidSchedulers.mainThread());
+
+//        taskObservable.subscribe(new Observer<Task>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d("aaa", "onSubscribe: " + Thread.currentThread().getName());
+//            }
+//
+//            @Override
+//            public void onNext(Task task) {
+//                Log.d("aaa", "onNext: " + Thread.currentThread().getName());
+//                Log.d("aaa", "onNext: " + task.getDescription());
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.d("aaa", "onError: " + e);
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d("aaa", "onComplete: ");
+//            }
+//        });
+
+        // Flowable
+        Flowable.range(0, 1000000)
+                .onBackpressureBuffer()
+                .observeOn(Schedulers.computation())
+                .subscribe(new FlowableSubscriber<Integer>() {
                     @Override
-                    public boolean test(Task task) {
-                        Log.d("aaa", "test: " + Thread.currentThread().getName());
-                        Log.d("aaa", "test: " + task.getDescription());
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return task.isComplete();
+                    public void onSubscribe(Subscription s) {
+
                     }
-                })
-                .observeOn(AndroidSchedulers.mainThread());
 
-        taskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d("aaa", "onSubscribe: " + Thread.currentThread().getName());
-            }
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d("aaa", "onNext: " + integer);
+                    }
 
-            @Override
-            public void onNext(Task task) {
-                Log.d("aaa", "onNext: " + Thread.currentThread().getName());
-                Log.d("aaa", "onNext: " + task.getDescription());
-            }
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.d("aaa", "onError: " + t);
+                    }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.d("aaa", "onError: " + e);
-            }
+                    @Override
+                    public void onComplete() {
 
-            @Override
-            public void onComplete() {
-                Log.d("aaa", "onComplete: ");
-            }
-        });
+                    }
+                });
     }
 }
